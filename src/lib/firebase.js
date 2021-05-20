59fcbb02c7d3df321805672359adbff8fd61848c
thanhhff-nezumi <ec2-user@ip-172-31-15-235.ec2.internal>
2021-05-20 15:36:46 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDbeORb2U5VuCc525zl_rWx9vqTiwM8EiI",
+  authDomain: "thanhhff-firebase-sample.firebaseapp.com",
+  projectId: "thanhhff-firebase-sample",
+  storageBucket: "thanhhff-firebase-sample.appspot.com",
+  messagingSenderId: "829487628826",
+  appId: "1:829487628826:web:f1927b3123035b2e1ec7a3"
+};
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+ 
